4b20fcb86f4fd0006da67f00a8a149c529f433dc
EliverLara <eliverlara@gmail.com>
2019-04-23 14:57:32 -0500

    Add attachments handler

--- app.js
+++ app.js
@@ -86,7 +86,38 @@ function handleMessage(sender_psid, received_message) {
         response = {
             "text": `You sent the message: "${received_message.text}". Now send me an image!`
         }
-    }  
+    } else if (received_message.attachments) {
+  
+        // Gets the URL of the message attachment
+        let attachment_url = received_message.attachments[0].payload.url;
+
+        response = {
+            "attachment": {
+              "type": "template",
+              "payload": {
+                "template_type": "generic",
+                "elements": [{
+                  "title": "Is this the right picture?",
+                  "subtitle": "Tap a button to answer.",
+                  "image_url": attachment_url,
+                  "buttons": [
+                    {
+                      "type": "postback",
+                      "title": "Yes!",
+                      "payload": "yes",
+                    },
+                    {
+                      "type": "postback",
+                      "title": "No!",
+                      "payload": "no",
+                    }
+                  ],
+                }]
+              }
+            }
+          }
+      
+    } 
   
     // Sends the response message
     callSendAPI(sender_psid, response);  
